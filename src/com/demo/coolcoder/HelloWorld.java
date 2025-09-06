09892c8d20cb1b21a630c743f40a434067f7f9c8
chanduujjina129 <ujjinachnadrasekhar@gmail.com>
2025-09-06 08:57:56 +0530

    This is my first commit

---
+++ src/com/demo/coolcoder/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.demo.coolcoder;
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {// main,JVM Conatiner //Helloword.class
+		System.out.println("inside main method");
+	}
+
+}
